efa01a7ac009a1272debb3e3376605a84b04a4ae
MBrauna <michelbrauna@gmail.com>
2020-09-19 17:12:52 -0300

    New migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,32 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+    public function up() {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->boolean('admin')->default(false);
+            $table->text('photo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->text('license')->nullable();
             $table->string('password');
+            $table->integer('id_user_recommend')->nullable();
+            $table->boolean('admin')->default(false);
+            $table->boolean('broker')->default(false);
+            $table->boolean('realtor')->default(false);
+            $table->double('percent',12,2)->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('users');
     }
 }
--- database/migrations/2020_09_16_003024_old_data.php
+++ database/migrations/2020_09_16_003024_old_data.php
@@ -4,19 +4,55 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class OldData extends Migration
-{
+class OldData extends Migration {
     public function up() {
-        Schema::create('quick_access',function(Blueprint $table){
-            $table->increments('id_quick_access');
-            $table->text('description');
-            $table->text('url');
-            $table->boolean('status');
+        Schema::create('user_address',function(Blueprint $table){
+            $table->increments('id_users_address');
+            $table->integer('id_user');
+            $table->string('address',150);
+            $table->string('city',150);
+            $table->string('state',150);
+            $table->string('country',150);
+            $table->integer('postal_code');
             $table->timestamps();
 
-            $table->index(['description']);
-            $table->index(['url']);
-        }); // Schema::create('quick_access',function(Blueprint $table){ ... });
+            $table->foreign('id_user')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->index(['id_user']);
+            $table->index(['address']);
+            $table->index(['city']);
+            $table->index(['state']);
+            $table->index(['country']);
+            $table->index(['postal_code']);
+        });
+
+        Schema::create('user_phone',function(Blueprint $table){
+            $table->increments('id_users_phone');
+            $table->integer('id_user');
+            $table->integer('ddi')->default(1);
+            $table->integer('ddd')->nullable();
+            $table->integer('phone');
+            $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->index(['id_user']);
+            $table->index(['ddi']);
+            $table->index(['ddd']);
+            $table->index(['phone']);
+        });
+
+        Schema::create('user_compensation',function(Blueprint $table){
+            $table->increments('id_user_compensation');
+            $table->integer('id_user');
+            $table->integer('type')->default(0); // [1] - Sales, [2] - Rental
+            $table->double('percentual',12,2)->default(0);
+            $table->dateTime('init_date');
+            $table->deteTime('end_date')->nullable();
+            $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        }); // Schema::create('user_compensation',function(Blueprint $table){ .. });
 
         Schema::create('archive',function(Blueprint $table){
             $table->increments('id_archive');
@@ -33,6 +69,8 @@ class OldData extends Migration
             $table->double('length',12,2)->nullable();
             $table->timestamps();
 
+            $table->foreign('id_user')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+
             $table->index(['id_user']);
             $table->index(['id_group']);
             $table->index(['id_contract']);
@@ -45,34 +83,119 @@ class OldData extends Migration
             $table->index(['length']);
         }); // Schema::create('archive',function(Blueprint $table){ .... });
 
-        Schema::create('users_group',function(Blueprint $table){
-            $table->increments('id_users_group');
-            $table->integer('id_responsible')->nullable();
+
+        Schema::create('quick_access',function(Blueprint $table){
+            $table->increments('id_quick_access');
+            $table->text('description');
+            $table->text('url');
+            $table->boolean('status');
+            $table->timestamps();
+
+            $table->index(['description']);
+            $table->index(['url']);
+        }); // Schema::create('quick_access',function(Blueprint $table){ ... });
+
+        Schema::create('group',function(Blueprint $table){
+            $table->increments('id_group');
             $table->text('name');
             $table->text('icon')->default('fas fa-users');
-            $table->double('percent',8,2);
             $table->boolean('status')->default(true);
             $table->timestamps();
 
             $table->index(['name']);
             $table->index(['status']);
-            $table->index(['id_responsible']);
         }); // Schema::create('archive',function(Blueprint $table){ .... });
 
-        Schema::create('users_group_user',function(Blueprint $table){
-            $table->increments('id_users_group_user');
-            $table->integer('id_users_group');
-            $table->integer('id_user');
+        Schema::create('contract',function(Blueprint $table){
+            $table->increments('id_contract');
+            $table->integer('type')->default(0); // [0] - Venda, [1] - Aluguel
+            $table->dateTime('start_contract');
+            $table->dateTime('end_contract')->nullable();
+            $table->dateTime('payment_date');
+            $table->boolean('payment_exec')->default(false);
+            $table->double('value',12,2)->default(0);
+            $table->integer('id_user_seller');
+            $table->text('description')->nullable();
             $table->timestamps();
 
+            $table->foreign('id_user_seller')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->index(['type']);
+            $table->index(['contract_date']);
+            $table->index(['payment_date']);
+            $table->index(['payment']);
+            $table->index(['id_user_seller']);
+        });
+
+        Schema::create('contract_address',function(Blueprint $table){
+            $table->increments('id_contract_address');
+            $table->integer('id_contract');
+            $table->string('address',150);
+            $table->string('city',150);
+            $table->string('state',150);
+            $table->string('country',150);
+            $table->integer('postal_code');
+            $table->timestamps();
+
+            $table->foreign('id_contract')->references('id_contract')->on('contract')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->index(['id_contract']);
+            $table->index(['address']);
+            $table->index(['city']);
+            $table->index(['state']);
+            $table->index(['country']);
+            $table->index(['postal_code']);
+        });
+
+        Schema::create('contract_phone',function(Blueprint $table){
+            $table->increments('id_users_phone');
+            $table->integer('id_user');
+            $table->integer('ddi')->default(1);
+            $table->integer('ddd')->nullable();
+            $table->integer('phone');
+            $table->timestamps();
+
+            $table->foreign('id_contract')->references('id_contract')->on('contract')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->index(['id_contract']);
+            $table->index(['ddi']);
+            $table->index(['ddd']);
+            $table->index(['phone']);
+        });
+
+        Schema::create('payment',function(Blueprint $table){
+            $table->increments('id_payment');
+            $table->integer('id_contract');
+            $table->integer('id_user');
+            $table->double('value',12,2)->default(0);
+            $table->double('value_aditional',12,2)->default(0);
+            $table->double('comission',8,2)->default(0);
+            $table->double('percent_group',8,2)->default(0);
+            $table->double('percent_tree',8,2)->default(0);
+            $table->dateTime('payment_date');
+            $table->timestamps();
+
+            $table->index(['id_contract']);
             $table->index(['id_user']);
-        }); // Schema::create('archive',function(Blueprint $table){ .... });
-    }
+            $table->index(['payment_date']);
+
+            $table->foreign('id_contract')->references('id_contract')->on('contract')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('id_user')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+
+    } // public function up() { ... }
 
     public function down() {
-        Schema::dropIfExists('quick_access');
+        Schema::dropIfExists('user_address');
+        Schema::dropIfExists('user_phone');
+        Schema::dropIfExists('user_compensation');
         Schema::dropIfExists('archive');
-        Schema::dropIfExists('users_group');
-        Schema::dropIfExists('users_group_user');
-    }
+        Schema::dropIfExists('quick_access');
+        Schema::dropIfExists('group');
+        Schema::dropIfExists('contract');
+        Schema::dropIfExists('contract_address');
+        Schema::dropIfExists('contract_phone');
+        Schema::dropIfExists('payment');
+
+    } // public function down() { ... }
 }
