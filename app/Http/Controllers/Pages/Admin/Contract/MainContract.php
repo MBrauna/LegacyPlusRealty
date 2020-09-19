3dc046be9493db0ccf504fe2f157bda0b3bb84d2
MBrauna <michelbrauna@gmail.com>
2020-09-19 18:49:13 -0300

    Pages controller

---
+++ app/Http/Controllers/Pages/Admin/Contract/MainContract.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Pages\Admin\Contract;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainContract extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Pages/Admin/User/MainUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Pages\Admin\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainUser extends Controller
+{
+    //
+}
