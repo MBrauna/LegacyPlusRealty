420d0f5b4136a7e6b13d5b6a3169552860bd82d6
MBrauna <michelbrauna@gmail.com>
2020-10-03 15:22:44 -0300

    addition payment

--- app/Http/Controllers/Pages/Dashboard/MainDashboard.php
+++ app/Http/Controllers/Pages/Dashboard/MainDashboard.php
@@ -29,10 +29,10 @@
                 $dataContract[$key]->value      =   'US$ '.number_format($value->sum_comission,2,'.',',');
             } // foreach ($dataContract as $key => $value) { ... }
 
-            $sumMonth       =   Payment::where('payment_date','>=',Carbon::now()->subMonths(1))->where('id_user',Auth::user()->id)->where('payment_date','<=',Carbon::now())->selectRaw('(sum(payment.comission) + sum(payment.additional)) as comission');
-            $sumYear        =   Payment::where('payment_date','>=',Carbon::now()->subMonths(1))->where('id_user',Auth::user()->id)->where('payment_date','<=',Carbon::now())->selectRaw('(sum(payment.comission) + sum(payment.additional)) as comission');
-            $sumMonth       =   'US$ '.number_format($sumMonth,2,'.',',');
-            $sumYear        =   'US$ '.number_format($sumYear,2,'.',',');
+            $sumMonth       =   Payment::where('payment_date','>=',Carbon::now()->subMonths(1))->where('id_user',Auth::user()->id)->where('payment_date','<=',Carbon::now())->selectRaw('(sum(payment.comission) + sum(payment.additional)) as comission')->first();
+            $sumYear        =   Payment::where('payment_date','>=',Carbon::now()->subMonths(1))->where('id_user',Auth::user()->id)->where('payment_date','<=',Carbon::now())->selectRaw('(sum(payment.comission) + sum(payment.additional)) as comission')->first();
+            $sumMonth       =   'US$ '.number_format($sumMonth->comission,2,'.',',');
+            $sumYear        =   'US$ '.number_format($sumYear->comission,2,'.',',');
 
             $monthMin       =   Carbon::now()->subMonths(1)->format('m/d/Y');
             $monthMax       =   Carbon::now()->format('m/d/Y');
